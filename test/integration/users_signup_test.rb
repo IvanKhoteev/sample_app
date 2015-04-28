0149236842dfa99b50c8253fc09ac0cf98d19760
Ivan Khoteev <xxxmlhk@mail.ru>
2015-04-28 21:02:16 +0300

    Решены задачи части 7 (размер граватара, тесты формы регистрации, флэша)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
-                               email: "user@invalid",
-                               password:              "foo",
-                               password_confirmation: "bar" }
-    end
-    assert_template 'users/new'
-  end
+ # test "invalid signup information" do
+ #   get signup_path
+ #   assert_no_difference 'User.count' do
+ #     post users_path, user: { name:  "",
+ #                              email: "user@invalid",
+ #                              password:              "foo",
+ #                              password_confirmation: "bar" }
+ #   end
+ #   assert_template 'users/new'
+ # end
 
   test "valid signup information" do
     get signup_path
@@ -21,5 +21,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash[:success] == ""
   end
+  
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select 'div #error_explanation'
+    assert_select 'div .field_with_errors'
+  end
+
 end
